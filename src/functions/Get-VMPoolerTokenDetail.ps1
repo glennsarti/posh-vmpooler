076c82afdba6db2b8b2a46eaa72fa421c4d55c9c
Glenn Sarti <glennsarti@users.noreply.github.com>
2016-01-26 08:56:46 -0800

    Completed the TokenDetail function

--- src/functions/Get-VMPoolerTokenDetail.ps1
+++ src/functions/Get-VMPoolerTokenDetail.ps1
@@ -10,17 +10,30 @@ Function Get-VMPoolerTokenDetail {
   
   Process {
     $result = Invoke-VMPoolerAPI -route "token/$TokenID"
-$result | Out-GridView -Wait
-    # Get-Member -InputObject $result -MemberType NoteProperty | % {
-    #   $tokenName = $_.Name
-    #   Get-Member -InputObject ($result."$tokenName") -MemberType NoteProperty | % {
-    #     $propertyHash."$($_.Name)" = $result."$tokenName"."$($_.Name)" 
-    #   }
-    #         
-    #   Write-Output (New-Object -TypeName PSObject -Property $propertyHash)
-    # }
-    
-    Write-Output $result
+
+    Get-Member -InputObject $result -MemberType NoteProperty | % {
+      $tokenName = $_.Name
+      $objToken = ($result."$tokenName")
+      
+      Add-Member -InputObject $objToken -MemberType NoteProperty -Name 'TokenID' -Value $tokenName
+
+      $AllVMList = @()
+      Get-Member -InputObject ($objToken.vms) -MemberType NoteProperty | % {
+        $VMState = $_.Name
+        
+        $StateVMList = @()
+        $objToken.vms."$VMState" | % {
+          $VMName = $_.ToString()
+          
+          $AllVMList += $VMName
+          $StateVMList += $VMName
+        }
+        Add-Member -InputObject $objToken -MemberType NoteProperty -Name "VMs_$($VMState)" -Value $StateVMList
+      }
+      Add-Member -InputObject $objToken -MemberType NoteProperty -Name "VMs_all" -Value $AllVMList
+       
+      Write-Output $objToken
+    }
   }
   
   End {
