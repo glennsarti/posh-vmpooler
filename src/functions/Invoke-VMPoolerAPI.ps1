40b6efbea5269471c3c941f8118ca98c7ba7f79d
Glenn Sarti <glennsarti@users.noreply.github.com>
2016-07-20 13:43:19 -0700

    Added token based auth

--- src/functions/Invoke-VMPoolerAPI.ps1
+++ src/functions/Invoke-VMPoolerAPI.ps1
@@ -14,51 +14,85 @@ Add-type @"
 "@
 
 
-Function Invoke-VMPoolerAPI([string]$url = '',$route,$payload = $null,[switch]$NoAuth,[switch]$NoParseResponse) {
-  if ($URL -eq '') { $URL = $VMPoolerServiceURI }
-  if ($URL.EndsWith('/')) { $URL = $URL.SubString(0,$URL.Length - 1)}
+Function Invoke-VMPoolerAPI {
+  [cmdletBinding(SupportsShouldProcess=$false,ConfirmImpact='Low',DefaultParameterSetName='CredAuth')]
+  param (
+    [Parameter(Mandatory=$true)]
+    [string]$route
 
-  $props = @{
-    'URI' = "$url/$route"
-    'Method' = 'GET'
-    #'Headers' = @{ 'X-AUTH-TOKEN' = $VMPoolToken}
-    #'DisableKeepAlive' = $true
-  }
-  
-  if ($payload -ne $null) {
-    $props.Method = 'POST'
-    $props.Body = ConvertTo-JSON -InputObject $payload -Depth 10
-  }
-  
-  if (-not $NoAuth) {
-    if ($VMPoolCredential -eq [System.Management.Automation.PSCredential]::Empty) {
-      $newCred = Get-Credential -Username ($Env:USERNAME) -Message "Credential for VMPooler"
-      if ($newCred -eq $null) { Throw "Missing required credentials for VMPooler"; return }
-      $Script:VMPoolCredential = $newCred
+    ,[Parameter(Mandatory=$false)]
+    [object]$payload = $null
+
+    ,[Parameter(Mandatory=$false)]
+    [string]$Method = 'GET'
+
+    ,[Parameter(Mandatory=$true,ParameterSetName='NoAuth')]
+    [switch]$NoAuth
+
+    ,[Parameter(Mandatory=$true,ParameterSetName='TokenAuth')]
+    [switch]$TokenAuth
+
+    ,[Parameter(Mandatory=$false)]
+    [string]$url = ''
+
+    ,[Parameter(Mandatory=$false)]
+    [switch]$NoParseResponse
+  )
+
+  Process {
+    if ($URL -eq '') { $URL = $VMPoolerServiceURI }
+    if ($URL.EndsWith('/')) { $URL = $URL.SubString(0,$URL.Length - 1)}
+
+    $props = @{
+      'URI' = "$url/$route"
+      'Method' = $Method
     }
     
-    $props.Credential = $VMPoolCredential
-  }
+    if ($payload -ne $null) {
+      if ($Method -eq 'GET') { $props.Method = 'POST' }
+      $props.Body = ConvertTo-JSON -InputObject $payload -Depth 10
+    }
     
-  [System.Net.ServicePointManager]::CertificatePolicy = new-object IDontCarePolicy 
-  $response = Invoke-WebRequest @props -Verbose
+    switch ($PSCmdlet.ParameterSetName) {
+      'NoAuth' { } # Do Nothing
+      'CredAuth' {
+        if ($VMPoolCredential -eq [System.Management.Automation.PSCredential]::Empty) {
+          $newCred = Get-Credential -Username ($Env:USERNAME) -Message "Credential for VMPooler"
+          if ($newCred -eq $null) { Throw "Missing required credentials for VMPooler"; return }
+          $Script:VMPoolCredential = $newCred
+        }
+        
+        $props.Credential = $VMPoolCredential
+      }
+      'TokenAuth' {
+        if ($Script:VMPoolToken -eq $TokenID -eq '') {
+          $Script:VMPoolToken = (Get-VMPoolerToken | Select -First 1).TokenID
+        }
+        $props.Headers = @{ 'X-AUTH-TOKEN' = "$($Script:VMPoolToken)" }
+      }
+      default { Throw "No idea what $($PSCmdlet.ParameterSetName) is" }
+    }
 
-  $objResponse = ($response.Content | ConvertFrom-JSON)
-  
-  if ($NoParseResponse)
-  {
-    return $objResponse
+    [System.Net.ServicePointManager]::CertificatePolicy = new-object IDontCarePolicy 
+    $response = Invoke-WebRequest @props -Verbose
+
+    $objResponse = ($response.Content | ConvertFrom-JSON)
+    
+    if ($NoParseResponse)
+    {
+      return $objResponse
+    }
+    
+    if (-not $objResponse.ok)
+    {
+      Write-Error "Error while invoking Pooler API"
+      $objResponse = $null
+    }
+    else
+    {
+      $objResponse.PSObject.Properties.Remove('ok')
+    }
+    
+    Write-Output $objResponse
   }
-  
-  if (-not $objResponse.ok)
-  {
-    Write-Error "Error while invoking Pooler API"
-    $objResponse = $null
-  }
-  else
-  {
-    $objResponse.PSObject.Properties.Remove('ok')
-  }
-  
-  Write-Output $objResponse
 }
