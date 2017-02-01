c34825d5b48c0eb406f010e383ed1722a26b17ff
Glenn Sarti <glennsarti@users.noreply.github.com>
2017-02-01 10:41:15 -0800

    Add ability to request a token

--- src/functions/New-VMPoolerToken.ps1
+++ src/functions/New-VMPoolerToken.ps1
@@ -7,9 +7,60 @@ function New-VMPoolerToken {
   }
   
   Process {
-    Throw "Not implemented"
+    try {
+      $result = Invoke-VMPoolerAPI -Method 'POST' -route 'token'
+      
+      if ($result.token -ne '') { Get-VMPoolerTokenDetail -Token $result.token}
+    }
+    catch {
+      Throw "Unable to get a new token $_"
+    }
   }
   
   End {
   }
 }
+
+# Function Get-VMPoolerToken {
+#   [cmdletBinding(SupportsShouldProcess=$false,ConfirmImpact='Low')]
+#   param (
+#     #[Parameter(Mandatory=$false,ValueFromPipeline=$true)]
+#     #[String]$Filter = ".*"
+#   )
+  
+#   Begin {   
+#   }
+  
+#   Process {
+#     if (($Script:VMPoolCredential -eq [System.Management.Automation.PSCredential]::Empty) -and ($Script:VMPoolToken -ne '')) {
+#       # Cached TokenID
+#       $propertyHash = @{ 'TokenID' = $Script:VMPoolToken }
+#       return (New-Object -TypeName PSObject -Property $propertyHash)      
+#     }
+
+#     try {
+#       $result = Invoke-VMPoolerAPI -route 'token'
+
+#       Get-Member -InputObject $result -MemberType NoteProperty | % {
+        
+#         $tokenName = $_.Name
+#         $objToken = $result."$tokenName"
+#         $propertyHash = @{ 'TokenID' = $tokenName }
+        
+#         $objToken | Get-Member -MemberType NoteProperty | % {
+#           $propertyHash."$($_.Name)" = $objToken."$($_.Name)" 
+#         }
+              
+#         Write-Output (New-Object -TypeName PSObject -Property $propertyHash)
+#       }
+#     }
+#     catch {
+#       # Errors here indicate either the use has no tokens assigned
+#       # or bad username password
+#     }
+#   }
+  
+#   End {
+#   }
+  
+# }
